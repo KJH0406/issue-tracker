9336ffa77151c07f9ad880392034943d28ea491e
김장호 <rlawkdgh0406@naver.com>
2025-05-10 13:26:25 +0900

    refactor: import 모듈 정리

--- src/app/workspace/layout.tsx
+++ src/app/workspace/layout.tsx
@@ -1,5 +1,4 @@
 import { Header } from "@/components/layout/Header"
-import { Sidebar } from "@/components/layout/Sidebar"
 import { getAuthUser } from "@/lib/auth"
 import { redirect } from "next/navigation"
 import { ReactNode } from "react"
@@ -12,8 +11,9 @@ export default async function WorkspaceRootLayout({
 }) {
   const user = await getAuthUser()
 
-  // 인증된 사용자가 아니면 로그인 페이지로 리다이렉트
+  // 로그인 사용자가 아니면 로그인 페이지로 리다이렉트
   if (!user) redirect("/login")
+
   return (
     <div className="h-screen flex flex-col">
       <Header />
